c88ef377cbc7d19e65d880fefd2134ca2e1288be
kndoshn <4eswaq@gmail.com>
2019-03-20 16:08:55 +0900

    dash

--- mac-setup.sh
+++ mac-setup.sh
@@ -78,6 +78,7 @@ brew cask install appcleaner
 brew cask install clipy
 brew cask install paw
 brew cask install figma
+brew cask install dash
 
 brew tap brona/iproute2mac
 brew install iproute2mac
